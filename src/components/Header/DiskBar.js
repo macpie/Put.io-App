802d6ea39d9718446a1811e3437ac57322c34e34
Macpie <macpie59@icloud.com>
2017-06-16 15:49:28 -0700

    Diskbar: Fix unused vars

--- src/components/Header/DiskBar.js
+++ src/components/Header/DiskBar.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import Chip from 'material-ui/Chip';
-import * as Utils from '../../utils';
 
 export default class DiskBar extends React.Component {
     render() {
         const {disk} = this.props;
-
-        let value = Math.round(((100 * disk.used) / disk.size)),
-            color = 'white';
-
-        if (value >= 75) {
-            color = 'red';
-        } else if (value >= 50 && value < 75) {
-            color = 'orange';
-        } else {
-            color = 'green';
-        }
 
         return (
             <div id="DiskBar">
@@ -23,7 +11,7 @@ export default class DiskBar extends React.Component {
                     style={{backgroundColor: 'rgb(255, 64, 129)'}}
                     labelStyle={{fontWeight: 'bold', color: 'white'}}
                 >
-                    {value + ' %'}
+                    {Math.round(((100 * disk.used) / disk.size)) + ' %'}
                 </Chip>
           </div>
         );
