dac9fdba0018ea1f4f8fcca3af3f8d7b2cfe0576
Macpie <macpie59@icloud.com>
2017-06-19 22:14:17 -0700

    FIX: NaN issue  for diskbar

--- src/components/Header/DiskBar.js
+++ src/components/Header/DiskBar.js
@@ -5,15 +5,19 @@ export default class DiskBar extends React.Component {
     render() {
         const {disk} = this.props;
 
-        return (
-            <div id="DiskBar">
-                <Chip
-                    style={{backgroundColor: 'rgb(255, 64, 129)'}}
-                    labelStyle={{fontWeight: 'bold', color: 'white'}}
-                >
-                    {Math.round(((100 * disk.used) / disk.size)) + ' %'}
-                </Chip>
-          </div>
-        );
+        if(disk && disk.used) {
+            return (
+                <div id="DiskBar">
+                    <Chip
+                        style={{backgroundColor: 'rgb(255, 64, 129)'}}
+                        labelStyle={{fontWeight: 'bold', color: 'white'}}
+                    >
+                        {Math.round(((100 * disk.used) / disk.size)) + ' %'}
+                    </Chip>
+              </div>
+            );
+        } else {
+            return null;
+        }
     }
 };
